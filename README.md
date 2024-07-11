7c6f98f63059b6ffe55eb82248a9f47d93446c5f
Dylan LaFontaine <dylan.j.lafontaine@gmail.com>
2024-07-10 22:27:57 -0700

    Added skeleton for an installable Python project

---
+++ README.md
@@ -0,0 +1,2 @@
+## Sample Installation Python Project
+This project is a skeleton for an installable python project. Modify the skeleton as needed to fit your project's needs.
---
+++ setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup
+
+if __name__ == "__main__":
+    # Shim used to allow for editable install.
+    # pyproject.toml is the actual setup file.
+    setup()
